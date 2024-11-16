12ad393a595809f9db1c3f3351ae4f68c4244dbc
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-16 12:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let res={};
+	for let i of str{
+		res[i]=(res[i]||0)+1;
+	}
+	for(let i of str){
+		if(res[i]===1){
+			return i;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
